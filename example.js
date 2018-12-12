6493d07da723b006796b913d8d6317aa104dfc1e
Fradniev <fradniev@hotmail.com>
2018-12-12 19:42:32 -0300

    Now saves on a text file the names of the author, and making to go through multiple pages

--- example.js
+++ example.js
@@ -1,18 +1,46 @@
 const puppeteer = require('puppeteer');
-const pevents = [
-    'response'
-];
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    var height=600;
+    var width=1200;
+    await page.setViewport({height, width});
 
     await page.goto('https://www.forbes.com/');
+    await scrollDown(page);
+    const authorsNames = await page.evaluate(() => [...document.documentElement.querySelectorAll('.byline__author-name')].map(elem => elem.innerText));
+    const authorsType = await page.evaluate(() => [...document.documentElement.querySelectorAll('.byline__author-type')].map(elem => elem.innerText));
+    await save(page,authorsNames,authorsType);
+    const topLinks = await page.evaluate(() => [...document.documentElement.querySelectorAll('a.header__title')].map(elem => elem.href));
+    for (let index = 0; index < topLinks.length; index++) {
+        console.log(topLinks[index])
+    }
 
+    await browser.close();
+})();
+
+function save(page,authorsNames,authorsType){
+    let authorList = {};
+    authorsNames.forEach((key, i) => authorList[key]=authorsType[i]);
+    console.log(authorList);
+    let authorsString= "";
+    for (var key in authorList) {
+        authorsString+=key+": "+authorList[key]+"\r\n";
+    }
+    fs.writeFile("test.txt", authorsString, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+}
+
+function scrollDown(page){
     console.log('Scrolling through page');
-
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
+    return page.evaluate(async () => {
+        return new Promise((resolve, reject) => {
             try {
                 const maxScroll = Number.MAX_SAFE_INTEGER;
                 let lastScroll = 0;
@@ -21,6 +49,7 @@ const pevents = [
                     const scrollTop = document.documentElement.scrollTop;
                     if (scrollTop === maxScroll || scrollTop === lastScroll) {
                         clearInterval(interval);
+                        console.log('Finished scrolling');
                         resolve();
                     } else {
                         lastScroll = scrollTop;
@@ -32,12 +61,4 @@ const pevents = [
             }
         });
     });
-    
-    console.log('Finished scrolling');
-    const authorsNames = await page.evaluate(() => [...document.documentElement.querySelectorAll('.byline__author-name')].map(elem => elem.innerText));
-    const authorsType = await page.evaluate(() => [...document.documentElement.querySelectorAll('.byline__author-type')].map(elem => elem.innerText));
-    let authorList = {};
-    authorsNames.forEach((key, i) => authorList[key]=authorsType[i]);
-    console.log(authorList);
-    await browser.close();
-})();
+}
