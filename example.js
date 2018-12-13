71f9d9a0c04d65edcc13bf851470c6eb386d7ec3
fradniev <fradniev@hotmail.com>
2018-12-13 18:37:09 -0300

    Added Jquery and added navigation through all navbar collecting names

--- example.js
+++ example.js
@@ -1,27 +1,33 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-
+const list=["billionare", "innovation", "leadership", "money", "consumer", "industry", "lifestyle", "lists"];
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    page.setDefaultNavigationTimeout("60000");
     var height=600;
     var width=1200;
     await page.setViewport({height, width});
 
-    await page.goto('https://www.forbes.com/');
+    await page.goto('https://www.forbes.com/', {waitUntil: "networkidle2"});
+    await page.addScriptTag({url:"https://code.jquery.com/jquery-3.2.1.min.js"});
     await scrollDown(page);
-    const authorsNames = await page.evaluate(() => [...document.documentElement.querySelectorAll('.byline__author-name')].map(elem => elem.innerText));
-    const authorsType = await page.evaluate(() => [...document.documentElement.querySelectorAll('.byline__author-type')].map(elem => elem.innerText));
-    await save(page,authorsNames,authorsType);
-    const topLinks = await page.evaluate(() => [...document.documentElement.querySelectorAll('a.header__title')].map(elem => elem.href));
+    const authorsNames = await page.evaluate(() => $(".byline__author-name").map(function(i,el) { return $(el).text(); }).get());
+    const authorsType = await page.evaluate(() =>$(".byline__author-type").map(function(i,el) { return $(el).text(); }).get());
+    await save(page,authorsNames,authorsType, "index");
+    const topLinks = await page.evaluate(() => $("a.header__title").map(function(i,el) { return $(el).attr('href'); }).get());
     for (let index = 0; index < topLinks.length; index++) {
-        console.log(topLinks[index])
+        await page.goto(topLinks[index], {waitUntil: "networkidle2"});
+        await scrollDown(page);
+        const authorsNames = await page.evaluate(() => $(".name-desc>a").map(function(i,el) { return $(el).text(); }).get());
+        const authorsType = await page.evaluate(() =>$(".atype>span").map(function(i,el) { return $(el).text(); }).get());
+        await save(page,authorsNames,authorsType, list[index]);
     }
 
     await browser.close();
 })();
 
-function save(page,authorsNames,authorsType){
+function save(page,authorsNames,authorsType, name){
     let authorList = {};
     authorsNames.forEach((key, i) => authorList[key]=authorsType[i]);
     console.log(authorList);
@@ -29,7 +35,7 @@ function save(page,authorsNames,authorsType){
     for (var key in authorList) {
         authorsString+=key+": "+authorList[key]+"\r\n";
     }
-    fs.writeFile("test.txt", authorsString, function(err) {
+    fs.writeFile(name+".txt", authorsString, function(err) {
         if(err) {
             return console.log(err);
         }
